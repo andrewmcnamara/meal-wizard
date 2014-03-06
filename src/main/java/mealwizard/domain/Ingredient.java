2e41f7bccdcbb3ccf2bcef13d1006b3734c6f4ab
Andrew McNamara <andrew.mcnamara@gmail.com>
2014-03-06 22:36:54 +1100

    Initial project -

---
+++ src/main/java/mealwizard/domain/Ingredient.java
@@ -0,0 +1,7 @@
+package mealwizard.domain;
+
+/**
+ * Created by andrew on 6/03/2014.
+ */
+public class Ingredient {
+}
---
+++ src/main/java/mealwizard/repositories/IngredientRepository.java
@@ -0,0 +1,7 @@
+package mealwizard.repositories;
+
+/**
+ * Created by andrew on 6/03/2014.
+ */
+public class IngredientRepository {
+}
